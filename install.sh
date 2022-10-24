6129e2fc7e3e3d58e938d3581ca2b6ef7b809c3d
Azu0925 <yamaneko564783@gmail.com>
2022-10-24 10:48:27 +0900

    feature: シンボリック

--- install.sh
+++ install.sh
@@ -1,12 +1,27 @@
-#!bin/bash
+#!/bin/bash -e
+
+IGNORE_PATTERN="^\.(git|travis|gitignore)"
 
 echo '~~~~~ install dotfiles ~~~~~'
+echo 'シンボリックリンク作成'
 
-cp ./.bashrc ~/.bashrc
+BASEDIR=$(dirname $0)
+cd $BASEDIR
 
-cp -r ./nvim ~/.config/
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
-rm -rf ./nvim.appimage
+for dotfile in .??*; do
+    if [ $dotfile = '.git' ]; then
+        continue
+    elif [ $dotfile = '.gitignore' ]; then
+        continue
+    else
+        ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+    fi
+done
+
+
+# cp -r ./nvim ~/.config/
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+# chmod u+x nvim.appimage
+# ./nvim.appimage
+# rm -rf ./nvim.appimage
 echo '[success]Neovim'
